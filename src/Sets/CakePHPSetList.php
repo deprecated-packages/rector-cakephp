36a1734f256ff596f7d88ca227f6dfc0a53daa06
TomasVotruba <tomas.vot@gmail.com>
2021-03-20 03:08:58 +0100

    add CakePHPSetList

--- src/Sets/CakePHPSetList.php
+++ src/Sets/CakePHPSetList.php
@@ -8,5 +8,53 @@ use Rector\Set\Contract\SetListInterface;
 
 final class CakePHPSetList implements SetListInterface
 {
+    /**
+     * @var string
+     */
+    public const CAKEPHP_30 = __DIR__ . '/../../config/sets/cakephp30.php';
 
+    /**
+     * @var string
+     */
+    public const CAKEPHP_34 = __DIR__ . '/../../config/sets/cakephp34.php';
+
+    /**
+     * @var string
+     */
+    public const CAKEPHP_35 = __DIR__ . '/../../config/sets/cakephp35.php';
+
+    /**
+     * @var string
+     */
+    public const CAKEPHP_36 = __DIR__ . '/../../config/sets/cakephp36.php';
+
+    /**
+     * @var string
+     */
+    public const CAKEPHP_37 = __DIR__ . '/../../config/sets/cakephp37.php';
+
+    /**
+     * @var string
+     */
+    public const CAKEPHP_38 = __DIR__ . '/../../config/sets/cakephp38.php';
+
+    /**
+     * @var string
+     */
+    public const CAKEPHP_40 = __DIR__ . '/../../config/sets/cakephp40.php';
+
+    /**
+     * @var string
+     */
+    public const CAKEPHP_41 = __DIR__ . '/../../config/sets/cakephp41.php';
+
+    /**
+     * @var string
+     */
+    public const CAKEPHP_42 = __DIR__ . '/../../config/sets/cakephp42.php';
+
+    /**
+     * @var string
+     */
+    public const CAKEPHP_FLUENT_OPTIONS = __DIR__ . '/../../config/sets/cakephp-fluent-options.php';
 }
