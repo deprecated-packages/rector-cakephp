74459724e305a4f6b3cdbe7a54442b342c216b7d
TomasVotruba <tomas.vot@gmail.com>
2021-03-20 03:43:14 +0100

    misc

--- config/config.php
+++ config/config.php
@@ -12,6 +12,6 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ->autowire()
         ->autoconfigure();
 
-    $services->load('Rector\\', __DIR__ . '/../src')
+    $services->load('Rector\\CakePHP\\', __DIR__ . '/../src')
         ->exclude([__DIR__ . '/../src/*/{Rector,ValueObject,Contract}']);
 };
