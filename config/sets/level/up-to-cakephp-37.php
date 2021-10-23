c14724b00505bd534283347712a4e49ca4a34375
Tomas Votruba <tomas.vot@gmail.com>
2021-10-23 16:29:49 +0200

    Add CakePHPLevelSetList (#10)

---
+++ config/sets/level/up-to-cakephp-37.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(\Rector\CakePHP\Set\CakePHPSetList::CAKEPHP_37);
+    $containerConfigurator->import(\Rector\CakePHP\Set\CakePHPLevelSetList::UP_TO_CAKEPHP_36);
+};
