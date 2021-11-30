115c1b4c678a133d08ee74c9e2a9393a83eadd16
Tomas Votruba <tomas.vot@gmail.com>
2021-11-30 20:16:04 +0300

    [DX] Make use of configure() method (#12)

--- config/sets/cakephp-fluent-options.php
+++ config/sets/cakephp-fluent-options.php
@@ -6,14 +6,13 @@ use Rector\CakePHP\Rector\MethodCall\ArrayToFluentCallRector;
 use Rector\CakePHP\ValueObject\ArrayToFluentCall;
 use Rector\CakePHP\ValueObject\FactoryMethod;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
 
     $services->set(ArrayToFluentCallRector::class)
-        ->call('configure', [[
-            ArrayToFluentCallRector::ARRAYS_TO_FLUENT_CALLS => ValueObjectInliner::inline([
+        ->configure([
+            ArrayToFluentCallRector::ARRAYS_TO_FLUENT_CALLS => [
                 new ArrayToFluentCall('Cake\ORM\Association', [
                     'bindingKey' => 'setBindingKey',
                     'cascadeCallbacks' => 'setCascadeCallbacks',
@@ -78,13 +77,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
                     'contain' => 'contain',
                     'page' => 'page',
                 ]),
-            ]),
-            ArrayToFluentCallRector::FACTORY_METHODS => ValueObjectInliner::inline([
+            ],
+            ArrayToFluentCallRector::FACTORY_METHODS => [
                 new FactoryMethod('Cake\ORM\Table', 'belongsTo', 'Cake\ORM\Association', 2),
                 new FactoryMethod('Cake\ORM\Table', 'belongsToMany', 'Cake\ORM\Association', 2),
                 new FactoryMethod('Cake\ORM\Table', 'hasMany', 'Cake\ORM\Association', 2),
                 new FactoryMethod('Cake\ORM\Table', 'hasOne', 'Cake\ORM\Association', 2),
                 new FactoryMethod('Cake\ORM\Table', 'find', 'Cake\ORM\Query', 2),
-            ]),
-        ]]);
+            ],
+        ]);
 };
--- tests/Rector/MethodCall/ArrayToFluentCallRector/config/configured_rule.php
+++ tests/Rector/MethodCall/ArrayToFluentCallRector/config/configured_rule.php
@@ -8,22 +8,21 @@ use Rector\CakePHP\Tests\Rector\MethodCall\ArrayToFluentCallRector\Source\Factor
 use Rector\CakePHP\ValueObject\ArrayToFluentCall;
 use Rector\CakePHP\ValueObject\FactoryMethod;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\SymfonyPhpConfig\ValueObjectInliner;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/../../../../../config/config.php');
 
     $services = $containerConfigurator->services();
     $services->set(ArrayToFluentCallRector::class)
-        ->call('configure', [[
-            ArrayToFluentCallRector::ARRAYS_TO_FLUENT_CALLS => ValueObjectInliner::inline([
+        ->configure([
+            ArrayToFluentCallRector::ARRAYS_TO_FLUENT_CALLS => [
                 new ArrayToFluentCall(ConfigurableClass::class, [
                     'name' => 'setName',
                     'size' => 'setSize',
                 ]),
-            ]),
-            ArrayToFluentCallRector::FACTORY_METHODS => ValueObjectInliner::inline([
+            ],
+            ArrayToFluentCallRector::FACTORY_METHODS => [
                 new FactoryMethod(FactoryClass::class, 'buildClass', ConfigurableClass::class, 2),
-            ]),
-        ]]);
+            ],
+        ]);
 };
