584d84bb1a8357423858ab2347249e50b1ff4803
Tomas Votruba <tomas.vot@gmail.com>
2021-12-08 10:38:23 +0300

    [DX] Replace deprecated danielstjules/stringy with Symfony\String (#14)

--- src/Rector/Property/ChangeSnakedFixtureNameToPascalRector.php
+++ src/Rector/Property/ChangeSnakedFixtureNameToPascalRector.php
@@ -12,7 +12,7 @@ use PhpParser\Node\Stmt\ClassLike;
 use PhpParser\Node\Stmt\Property;
 use PhpParser\Node\Stmt\PropertyProperty;
 use Rector\Core\Rector\AbstractRector;
-use Stringy\Stringy;
+use Symfony\Component\String\UnicodeString;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
@@ -108,8 +108,8 @@ CODE_SAMPLE
 
         $pascalCaseTableParts = array_map(
             function (string $token): string {
-                $stringy = new Stringy($token);
-                return (string) $stringy->upperCamelize();
+                $tokenUnicodeString = new UnicodeString($token);
+                return ucfirst($tokenUnicodeString->camel()->toString());
             },
             $tableParts
         );
